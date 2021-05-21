c5822c97d575937a21dc0facd2cbde3607d68f52
TTVTrob <82639775+TTVTrob@users.noreply.github.com>
2021-05-21 13:35:05 +0530

    Add files via upload

---
+++ js/Launcher.js
@@ -0,0 +1,29 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+    fly(){
+        this.launcher.bodyA = null;
+    }
+    attach(bodyA){
+        this.launcher.bodyA = bodyA;      
+    }
+   display(){if(this.launcher.bodyA ){
+        push();
+        var pointA = this.launcher.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        //stroke("lime")
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        pop();
+        }
+    }
+}
